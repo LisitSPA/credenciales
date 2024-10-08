396052e40def3f00d28546a3d75f666f5196e991
Gisela Urrels <g.urrels@vocalcom.com>
2024-10-08 06:57:01 -0300

    creación del desplegable para carga masiva

--- frontend/src/app/search-section/search-section.component.ts
+++ frontend/src/app/search-section/search-section.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-section',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Agrega FormsModule aquí
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-section.component.html',
   styleUrls: ['./search-section.component.css']
 })
 export class SearchSectionComponent {
   searchQuery: string = '';
+  isDropdownVisible: boolean = false; 
+  selectedOption: string | null = null; 
 
   onSearch() {
     console.log('Buscar:', this.searchQuery);
   }
 
   onUpload() {
+    this.isDropdownVisible = !this.isDropdownVisible; 
     console.log('Carga masiva de documentos');
+  }
+
+  onLoadOption(option: string) {
+    console.log('Opción seleccionada:', option);
+    this.selectedOption = option; 
+  }
+
+  onLoadExcel() {
+    console.log('Cargar excel o .zip');
   }
 }
