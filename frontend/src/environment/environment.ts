94d89510e575ecd6b78ebc40bc8b76fb9cf0f25b
Marley Vargas <mvargas@gmstek.com>
2024-10-30 07:47:16 -0500

    Cambio url api

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
     production: false,
     //apiUrl: 'applicationUrl": "https://localhost:5001;http://localhost:5000',
     //apiUrl: 'https://localhost:5002/api',
-    apiUrl : 'https://credenciales-api-evcvgchfgmguc3gf.canadacentral-01.azurewebsites.net/api/'
-
+    apiUrl : 'https://backend-credenciales-gtdqgnadc6efh7ee.scm.canadacentral-01.azurewebsites.net'
   };
   
