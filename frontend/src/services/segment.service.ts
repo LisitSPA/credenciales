2131289ed3871abcae8804814b3b36c5dbfa4849
Gisela Urrels <g.urrels@vocalcom.com>
2024-11-01 07:34:15 -0300

    modificación del servicio

--- frontend/src/services/segment.service.ts
+++ frontend/src/services/segment.service.ts
@@ -6,52 +6,76 @@ import { environment } from '../environment/environment';
   providedIn: 'root',
 })
 export class SegmentService {
-  private apiUrl = environment.apiUrl + "/segment";  
-  headers: any;
+  private apiUrl = `${environment.apiUrl}/segment`;
 
   constructor(private http: HttpClient) {}
 
-  private getHeaders(): HttpHeaders {
+  private createHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.error('No se encontró el token en localStorage.');
+      throw new Error('No se encontró el token.');
+    }
     return new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json', 
     });
   }
 
   uploadMissiveSegment(file: File): Promise<any> {
-    const headers = this.getHeaders();
-    const formData = new FormData();
-    formData.append('FileData', file);
-    return this.http.post(`${this.apiUrl}/UploadMassive`, formData, { headers }).toPromise();
+    try {
+      const headers = this.createHeaders();
+      const formData = new FormData();
+      formData.append('FileData', file);
+      return this.http.post(`${this.apiUrl}/UploadMassive`, formData, { headers }).toPromise();
+    } catch (error) {
+      console.error('Error al cargar archivo masivo:', error);
+      return Promise.reject(error);
+    }
   }
 
   getPaginatedSegments(page: number, pageSize: number): Promise<any> {
-    const headers = this.getHeaders();
-    const params = { page: page.toString(), pageSize: pageSize.toString() };
-    return this.http.get(`${this.apiUrl}/paginated`, { params, headers }).toPromise();
+    try {
+      const headers = this.createHeaders();
+      const params = { page: page.toString(), pageSize: pageSize.toString() };
+      return this.http.get(`${this.apiUrl}/paginated`, { params, headers }).toPromise();
+    } catch (error) {
+      console.error('Error al obtener los segmentos paginados:', error);
+      return Promise.reject(error);
+    }
   }
 
-  createSegment(nombreCompleto: string, colorSegmento: string): Promise<any> {
-    let headers = this.headers.set('Content-Type', 'application/json'); 
-    const payload = {
-      NombreCompleto: nombreCompleto,
-      Color: colorSegmento,
-    };
-  
-    return this.http.post(`${this.apiUrl}`, payload, { headers }).toPromise()
-      .then(response => {
-        console.log('Respuesta del servidor:', response);
-        return response;
-      })
-      .catch(error => {
-        console.error('Error en la creación del segmento en el servidor:', error);
-        throw error;
-      });
+  createSegment(nombreCompleto: string, colorSegmento: string, estadoSegmento: boolean): Promise<any> {
+    try {
+      const headers = this.createHeaders();
+      const payload = {
+        Name: nombreCompleto,
+        Color: colorSegmento,
+        Active: estadoSegmento,
+      };
+
+      return this.http.post(`${this.apiUrl}`, payload, { headers }).toPromise()
+        .then(response => {
+          console.log('Respuesta del servidor:', response);
+          return response;
+        })
+        .catch(error => {
+          console.error('Error en la creación del segmento en el servidor:', error);
+          throw error;
+        });
+    } catch (error) {
+      console.error('Error al intentar crear el encabezado o al crear el segmento:', error);
+      return Promise.reject(error);
+    }
   }
-  
-  
 
   deleteSegment(id: number): Promise<any> {
-    const headers = this.getHeaders();
-    return this.http.delete(`${this.apiUrl}/${id}`, { headers }).toPromise();
+    try {
+      const headers = this.createHeaders();
+      return this.http.delete(`${this.apiUrl}/${id}`, { headers }).toPromise();
+    } catch (error) {
+      console.error('Error al eliminar el segmento:', error);
+      return Promise.reject(error);
+    }
   }
 }
