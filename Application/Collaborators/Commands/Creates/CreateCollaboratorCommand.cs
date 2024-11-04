2c0931c73387a14ff9a75ec171ef569cd2661be2
Gisela Urrels <g.urrels@vocalcom.com>
2024-11-03 21:22:44 -0300

    .

--- Application/Collaborators/Commands/Creates/CreateCollaboratorCommand.cs
+++ Application/Collaborators/Commands/Creates/CreateCollaboratorCommand.cs
@@ -23,7 +23,7 @@ public record CreateCollaboratorCommand : IRequest<Response<int>>
 {
     public string CompleteName { get; set; }
     public string RUT { get; set; }
-    public int LeadershipId { get; set; }
+    public int? LeadershipId { get; set; }
     public int? SegmentId { get; set; }
     public string Position { get; set; }
     public string Sede { get; set; }
