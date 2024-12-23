59059469b1cbfcda5b95d43a66da327330b0a511
Marley Vargas <mvargas@gmstek.com>
2024-12-23 12:12:55 -0500

    test

--- frontend/src/app/credencialWeb/credencialWeb.ts
+++ frontend/src/app/credencialWeb/credencialWeb.ts
@@ -56,7 +56,7 @@ export class CredencialWebComponent implements OnInit {
     TEL:${this.celular}
     EMAIL:${this.correo}
     ADR:${this.sede}
-    ORG:${this.nombre}
+    ORG:Nombre ${this.nombre}
     TITLE:${this.cargo}
     END:VCARD`;
 
@@ -76,7 +76,7 @@ export class CredencialWebComponent implements OnInit {
     TEL:${this.celular}
     EMAIL:${this.correo}
     ADR:${this.sede}
-    ORG:${this.nombre}
+    ORG:Nombre ${this.nombre}
     TITLE:${this.cargo}
     END:VCARD`;
 
