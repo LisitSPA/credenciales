133bc13d8a313a7cb9bdf8c04e756e4848e37b07
Johan Samper <jsamper@lisit.cl>
2025-01-19 18:46:59 -0500

    Update Startup.cs

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -90,13 +90,10 @@ namespace Api
         {
             app.UseCors("CorsPolicy");
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
-            }
-
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+            
             app.Use(async (context, next) =>
             {
                 context.Response.Headers.Append("X-Frame-Options", "DENY"); 
