82c78523913db14b477f3b27423927d13c48d7a8
Johan Samper <jsamper@lisit.cl>
2025-02-03 04:06:42 -0500

    Update environment.ts

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:44394/api',
-    // apiUrl : 'https://back-credenciales-hehpdwc2f6gkd7dy.canadacentral-01.azurewebsites.net/api'
+    // apiUrl: 'https://localhost:44394/api',
+    apiUrl : 'https://back-credenciales-hehpdwc2f6gkd7dy.canadacentral-01.azurewebsites.net/api',
     encryptKey: '01234567890123456789012345678901' 
   };
   
